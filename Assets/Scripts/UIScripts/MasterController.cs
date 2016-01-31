e9be2236145c558f160e8b33f72a23cd926c0b03
peterwolf <peterawolf@gmail.com>
2016-01-30 19:41:25 -0500

    first real commit

---
+++ Assets/Scripts/UIScripts/MasterController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterController : MonoBehaviour {
+	
+
+	public enum GUIMode { TOP_LEVEL, TOWN_LIST, TOWN_DETAIL, PLOTTING, ARMY_DETAIL, ARMY_LIST };
+
+    private GUIMode guiMode = GUIMode.TOP_LEVEL;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setMode(GUIMode guiMode) {
+
+        this.guiMode = guiMode;
+    }
+	
+}
